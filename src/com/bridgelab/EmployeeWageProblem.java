46ba9de7a465eb08eef3b46775b4e2d7e517d4cd
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-22 22:27:38 +0530

    Added :Calculating monthly wages

--- src/com/bridgelab/EmployeeWageProblem.java
+++ src/com/bridgelab/EmployeeWageProblem.java
@@ -2,7 +2,7 @@ package com.bridgelab;
 import java.util.Random;
 
 public class EmployeeWageProblem {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage problem");
         //This program will check the employee is present or absent
         Random random = new Random();
@@ -10,21 +10,25 @@ public class EmployeeWageProblem {
         int WAGE_PER_HOUR = 20;
         int TOTAL_HOUR = 8;
         int PARTTIME_HOURS = 4;
-        int job_timing = random.nextInt(3);
+        int WORKING_DAY = 20;
+        int daily_wage=0;
+        int monthly_wages=0;
         //code
-        switch (job_timing) {
-            case 0:
-                System.out.println("Employee Present");
-                System.out.println("Employee is doing full time job");
-                System.out.println("Daily Wages = " + WAGE_PER_HOUR * TOTAL_HOUR);
-                break;
-            case 1:
-                System.out.println("Employee Present");
-                System.out.println("Employee is doing part time job");
-                System.out.println("Daily Wages = " + WAGE_PER_HOUR * PARTTIME_HOURS);
-                break;
-            case 2:
-                System.out.println("Employee Absent");
+
+        for (int days_count = 1; days_count <= WORKING_DAY; days_count++) {
+            int job_timing = random.nextInt(3);
+            switch (job_timing) {
+                case 1:
+                    daily_wage=WAGE_PER_HOUR * TOTAL_HOUR *days_count;
+                    break;
+                case 2:
+                    daily_wage=WAGE_PER_HOUR * PARTTIME_HOURS *days_count;
+                    break;
+                case 0:
+                    break;
+            }
+           monthly_wages=daily_wage+monthly_wages;
         }
+        System.out.println("Monthly Wages = " +monthly_wages);
     }
 }
