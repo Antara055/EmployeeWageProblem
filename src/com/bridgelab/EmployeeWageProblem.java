d31247a4c0322319e82a7120ef681d3478007330
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-22 21:25:35 +0530

    Added : Daily Wage

--- src/com/bridgelab/EmployeeWageProblem.java
+++ src/com/bridgelab/EmployeeWageProblem.java
@@ -7,9 +7,13 @@ public class EmployeeWageProblem {
         //This program will check the employee is present or absent
         Random random = new Random();
         //variable
+        int WAGE_PER_HOUR = 20;
+        int TOTAL_HOUR = 8;
         int r = random.nextInt(2);
-        if (r == 1)
+        if (r == 1) {
             System.out.println("Employee is present");
+            System.out.println("DayWage = " + WAGE_PER_HOUR * TOTAL_HOUR);
+        }
         else
             System.out.println("Employee is absent");
     }
