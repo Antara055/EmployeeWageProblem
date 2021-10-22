ab8905a04eedffa477be93f9af268aac3aacedd5
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-22 21:01:12 +0530

    Added : Checking Employee is present or absent

--- src/com/bridgelab/EmployeeWageProblem.java
+++ src/com/bridgelab/EmployeeWageProblem.java
@@ -1,7 +1,16 @@
 package com.bridgelab;
+import java.util.Random;
 
 public class EmployeeWageProblem {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage problem");
+        //This program will check the employee is present or absent
+        Random random = new Random();
+        //variable
+        int r = random.nextInt(2);
+        if (r == 1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
