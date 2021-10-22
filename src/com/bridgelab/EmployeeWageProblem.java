e798aafb5a8d302525b52648aeffea3d0f0e4429
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-22 21:42:38 +0530

    Added :Chwcking Employee doing Part time or Full Time

--- src/com/bridgelab/EmployeeWageProblem.java
+++ src/com/bridgelab/EmployeeWageProblem.java
@@ -9,10 +9,15 @@ public class EmployeeWageProblem {
         //variable
         int WAGE_PER_HOUR = 20;
         int TOTAL_HOUR = 8;
-        int r = random.nextInt(2);
-        if (r == 1) {
-            System.out.println("Employee is present");
-            System.out.println("DayWage = " + WAGE_PER_HOUR * TOTAL_HOUR);
+        int PARTTIME_HOURS = 4;
+        int job_timing = random.nextInt(3);
+        if (job_timing == 1) {
+            System.out.println("Employee is doing full time job");
+            System.out.println("Daily Wages = " + WAGE_PER_HOUR * TOTAL_HOUR);
+        }
+        else if (job_timing == 2) {
+            System.out.println("Employee is doing part time job");
+            System.out.println("Daily Wages = " + WAGE_PER_HOUR * PARTTIME_HOURS);
         }
         else
             System.out.println("Employee is absent");
